eeacb77a809686d43799f51047029ca58f5b49ec
troy <troywhitetw@ymail.com>
2019-03-28 21:33:16 +0800

    更新寫法

--- NativeAJAX.js
+++ NativeAJAX.js
@@ -1,45 +1,46 @@
 /**
  * @param sMethod {string} 'GET' or 'POST'
  * @param sUrl {string}
- * @param oParameter {JSON}
+ * @param oParameter {object}
  * @param fSuccessCallBack {function} callback function
  * @param fFailCallBack {function} callback function optional
  */
-function ajax(sMethod, sUrl, oParameter, fSuccessCallBack, fFailCallBack) {
-    // if (sMethod !== 'GET' || sMethod !== 'POST') {
-    //     return 'method error!!';
-    // }
+const ajax = (sMethod, sUrl, oParameter, fSuccessCallBack, fFailCallBack = null) => {
+    // if (sMethod !== 'GET' || sMethod !== 'POST') return 'method error!!';
 
     // 1. 創建 ajax 物件
-    let xhttp = new XMLHttpRequest();
+    const xhttp = new XMLHttpRequest();
 
     switch (sMethod) {
         case 'GET':
             if (oParameter !== null) {
-                sUrl += "?";
+                sUrl += '?';
                 for (let key in oParameter) {
-                    sUrl += key + "=" + oParameter[key] + "&";
+                    sUrl += `${key}=${oParameter[key]}&`;
                 }
                 sUrl = sUrl.substr(0, sUrl.length - 1);
             }
 
             // 2. 連接伺服器
             xhttp.open(sMethod, sUrl, true);
-
             // 3. 發送請求
             xhttp.send();
+
             break;
         case 'POST':
-            let sParameter = "";
+            let sParameter = '';
+
             if (oParameter !== null) {
                 for (let key in oParameter) {
-                    sParameter += key + "=" + oParameter[key] + "&";
+                    sParameter += `${key}=${oParameter[key]}&`;
                 }
                 sParameter = sParameter.substr(0, sParameter.length - 1);
             }
+
             xhttp.open(sMethod, sUrl, true);
-            xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
             xhttp.send(sParameter);
+
             break;
     }
 
@@ -55,4 +56,4 @@ function ajax(sMethod, sUrl, oParameter, fSuccessCallBack, fFailCallBack) {
             }
         }
     };
-}
+};
